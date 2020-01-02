fe01e9d6eb3387339da975a41a2e82614eb37244
Joseph A <josephananda88888@gmail.com>
2020-01-02 23:53:28 +0700

    sign in and sign up

---
+++ app/src/main/java/com/dscproject/cantinplusplus/SignInAct.java
@@ -0,0 +1,29 @@
+package com.dscproject.cantinplusplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignInAct extends AppCompatActivity {
+    TextView sign_up;
+    Button btn_sign_in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        sign_up = findViewById(R.id.sign_up);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotosignup = new Intent(SignInAct.this, SignUpAct.class);
+                startActivity(gotosignup);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/dscproject/cantinplusplus/SignUpAct.java
@@ -0,0 +1,29 @@
+package com.dscproject.cantinplusplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignUpAct extends AppCompatActivity {
+    TextView sign_in;
+    Button btn_sign_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        sign_in = findViewById(R.id.sign_in);
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotosignin = new Intent(SignUpAct.this, SignInAct.class);
+                startActivity(gotosignin);
+            }
+        });
+    }
+}
